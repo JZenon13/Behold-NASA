c6e17a87add62a9203cf5e2861b090c1e6a70ba4
Jesse Zenon <jz.the.dev@gmail.com>
2024-05-16 21:40:32 -0500

    api/docker env variables

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,28 @@
-﻿namespace NASA_BE;
+﻿using System;
+namespace NASA_BE;
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        string roverPhotoDates = "./api/dates.txt";
+        string[] dates = File.ReadAllLines(roverPhotoDates);
+
+
+        if (File.Exists(roverPhotoDates))
+        {
+            try
+            {
+                foreach (string date in dates)
+                {
+                    Console.WriteLine(date);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
     }
 }
---
+++ backend/api/marsRoverPhotos.cs
@@ -0,0 +1,4 @@
+
+
+var apiKey = Environment.GetEnvironmentVariable("API_KEY");
+var baseUrl = Environment.GetEnvironmentVariable("BASE_URL");
